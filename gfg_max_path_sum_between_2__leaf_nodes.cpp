6305606c679387d4c98e533ee0c081f89fc963c7
Mohammed qubaisuddin <hiq4dsa@gmail.com>
2021-07-15 20:21:25 +0530

    Max path sum

--- gfg_max_path_sum_between_2__leaf_nodes.cpp
+++ gfg_max_path_sum_between_2__leaf_nodes.cpp
@@ -1,22 +1,31 @@
-/*
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;   
-    Node(int x){
-        data = x;
-        left = right = NULL;
+class Pair{
+public:
+    int maxLeaves;
+    int maxSum;
+    Pair(){
+        this->maxLeaves=INT_MIN;
+        this->maxSum=0;
     }
 };
-*/
 class Solution {
 public:
-    int max_sum(Node*root){
-        if(!root) return 0;
-        return max(max_sum(root->left),max_sum(root->right))+root->data;
+    Pair max_sum(Node*root){
+        Pair ans;
+        if(!root){
+            return ans;
+        }
+        if(!root->left and !root->right){
+            ans.maxSum=root->data;
+            return ans;
+        }
+        Pair left=max_sum(root->left);
+        Pair right=max_sum(root->right);
+        ans.maxLeaves=max(max(left.maxLeaves,right.maxLeaves),left.maxSum+right.maxSum+root->data);
+        ans.maxSum=max(left.maxSum+root->data,right.maxSum+root->data);
+        return ans;
     }
     int maxPathSum(Node* root){
+        return max_sum(root).maxLeaves;
     }
-};
+}; 
 
